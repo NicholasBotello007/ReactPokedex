236afb8cbc8c7da50779781e23f864066a10c7ea
Nico <Nicholas.botello007@gmail.com>
2022-05-22 23:12:52 -0500

    day 2, trying to figure out things

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import './App.css';
 
 const App = () => {
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonType, setPokemonType] = useState("");
+
+  const getPokemon = async () => {
+    const toArray = [];
+    try{
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const res = await axios.get(url);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
+
   return (
     <div className="App">
         <h1>Hello</h1>
