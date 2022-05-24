ef372c59a92e8be9476faea1c366a895d682d1b7
Nico <Nicholas.botello007@gmail.com>
2022-05-23 22:52:41 -0500

    Skipped makeStyles and made the pokemon cards

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -3,41 +3,37 @@ import {
     AppBar,
     Toolbar,
     Grid,
-    Card
+    Card,
+    CardContent
 } from "@mui/material"
-import { makeStyles } from "@mui/styles";
 
-const useStyles = makeStyles({
-    pokedexContainer:{
-        paddingTop: '20px',
-        paddingLeft: '50px',
-        paddingRight: '50px'
-    },
-})
 
 const getPokemonCard = () => {
     return(
-    <Grid item xs = {12} sm = {4}>
-        <Card>hi</Card>
+    <Grid item xs = {4} sm = {4}>
+        <Card>
+            <CardContent>
+                Hi
+            </CardContent>
+        </Card>
     </Grid>
     )
 }
 
 const Pokedex = () => {
-    const classes = useStyles();
     
     return (
         <>
             <AppBar position = "static">
                 <Toolbar/>
             </AppBar>
-            <Grid container spacing = {2} className = {classes.pokedexContainer}>
-              <Grid item xs={5}>
-                  this is item 1
-                </Grid>  
-            <Grid item xs={5}>
-                  this is item 2
-             </Grid>
+            <Grid container spacing = {4}>
+                  {getPokemonCard()}
+                  {getPokemonCard()}
+                  {getPokemonCard()}
+                  {getPokemonCard()}
+                  {getPokemonCard()}
+                  {getPokemonCard()}
             </Grid>
         </>
     );
