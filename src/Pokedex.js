e568efdd4691308d2725c34c012300648cf17e61
Nico <Nicholas.botello007@gmail.com>
2022-05-23 22:42:30 -0500

    progress made for the grid layout (makestyles doesnt work tho)

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,7 +1,46 @@
 import React from "react";
+import {
+    AppBar,
+    Toolbar,
+    Grid,
+    Card
+} from "@mui/material"
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+    pokedexContainer:{
+        paddingTop: '20px',
+        paddingLeft: '50px',
+        paddingRight: '50px'
+    },
+})
+
+const getPokemonCard = () => {
+    return(
+    <Grid item xs = {12} sm = {4}>
+        <Card>hi</Card>
+    </Grid>
+    )
+}
 
 const Pokedex = () => {
-    return <div> this is the pokedex page</div>;
+    const classes = useStyles();
+    
+    return (
+        <>
+            <AppBar position = "static">
+                <Toolbar/>
+            </AppBar>
+            <Grid container spacing = {2} className = {classes.pokedexContainer}>
+              <Grid item xs={5}>
+                  this is item 1
+                </Grid>  
+            <Grid item xs={5}>
+                  this is item 2
+             </Grid>
+            </Grid>
+        </>
+    );
 };
 
 export default Pokedex;
