55be3e55dbaaebbf252a170f839b716d9b77a927
Nico <Nicholas.botello007@gmail.com>
2022-05-23 21:46:22 -0500

    another day of set-up

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import './App.css';
+import React from "react";
+import Pokedex from "./Pokedex";
+import Pokemon from "./Pokemon";
+import { Route, Switch } from "react-router-dom";
 
-const App = () => {
-  const [pokemon, setPokemon] = useState("pikachu");
-  const [pokemonData, setPokemonData] = useState([]);
-  const [pokemonType, setPokemonType] = useState("");
-
-  const getPokemon = async () => {
-    const toArray = [];
-    try{
-      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-      const res = await axios.get(url);
-      console.log(res);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    getPokemon();
-  }, []);
-
-
-  return (
-    <div className="App">
-        <h1>Hello</h1>
-    </div>
-  );
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/" render={(props) => <Pokedex {...props} />} />
+    <Route
+      exact
+      path="/:pokemonId"
+      render={(props) => <Pokemon {...props} />}
+    />
+  </Switch>
+);
 
 export default App;
