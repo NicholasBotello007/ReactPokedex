c5a1be6bd50caa797a023386ac2f938e85b2c5ae
Nico <Nicholas.botello007@gmail.com>
2022-05-25 21:47:56 -0500

    finished main app, needs more details

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
     AppBar,
     Toolbar,
@@ -7,20 +7,48 @@ import {
     CardMedia,
     CardContent,
     Typography,
+    TextField,
     CircularProgress
 } from "@mui/material"
-import mockData from "./mockData"
 import { toFirstCharUppercase } from "./constants";
+import SearchIcon from '@mui/icons-material/Search';
+import axios from "axios";
+
+
 
 const Pokedex = props => {
     const { history } = props;
-    const [pokemonData, setPokemonData] = useState(mockData);
+    const [pokemonData, setPokemonData] = useState({});
+    const [filter, setFilter] = useState("");
+
+    const handleSearchChange = (e) => {
+        setFilter(e.target.value);
+      };
+
+    useEffect(() => {
+        axios
+          .get(`https://pokeapi.co/api/v2/pokemon?limit=898`)
+          .then(function (response) {
+                const { data } = response;
+                const { results } = data;
+                const newPokemonData = {};
+                results.forEach((pokemon, index) => {
+                    newPokemonData[index + 1] = {
+                        id: index + 1,
+                        name: pokemon.name,
+                        sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                         index + 1
+                        }.png`,
+                    };
+                });
+                setPokemonData(newPokemonData);
+            });
+    }, []);
+
 
     const getPokemonCard = (pokemonId) => {
        console.log(pokemonData[pokemonId])
-        const { id, name } = pokemonData[pokemonId];
-        const sprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+id+".png"
-    
+        const { id, name, sprite } = pokemonData[pokemonId];
     
         return(
         <Grid item xs = {4} key = {pokemonId}>
@@ -40,11 +68,17 @@ const Pokedex = props => {
     return (
         <>
             <AppBar position = "static">
-                <Toolbar />
+                <Toolbar> 
+                    <div>
+                        <SearchIcon  />
+                        <TextField onChange={handleSearchChange} />
+                    </div>
+               </Toolbar>
             </AppBar>
             {pokemonData ? (
             <Grid container spacing = {4}>
-                  {Object.keys(pokemonData).map(pokemonId => 
+                  {Object.keys(pokemonData).map((pokemonId) => 
+                    pokemonData[pokemonId].name.includes(filter) &&
                     getPokemonCard(pokemonId))}
             </Grid>
             ) : (
